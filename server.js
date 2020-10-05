fc9a2924a3de884cca0c5749b1eb99111b1067c2
Nakulesh Jayakrishnan <nakuleshj1998@gmail.com>
2020-10-05 16:52:57 +0530

    fixes

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use('/api/post',postRouter);
 const uri=process.env.ATLAS_URI
 mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology: true});
 
-    app.use(express.static(path.join(__dirname,'./client/build')));
+    app.use(express.static(path.join(__dirname,'/client/build')));
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'./client/build','index.html'));
+        res.sendFile(path.resolve(__dirname,'/client/build','index.html'));
     });
 const port=process.env.PORT||5000;
 app.listen(port,()=>{
